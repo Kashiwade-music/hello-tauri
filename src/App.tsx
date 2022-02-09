dd08cf327213dcdac47753934bfffc5099a53520
kashiwade <music5d7_1415@outlook.com>
2022-02-09 11:48:39 +0900

    add basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,14 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
+import List from "@mui/material/List";
+import InboxIcon from "@mui/icons-material/Inbox";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import tauriWord from "./wordmark.svg";
 import "./App.css";
 
@@ -58,6 +66,10 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+const handleClick = () => {
+  console.info("You clicked the Chip.");
+};
+
 function App() {
   return (
     <div className="App">
@@ -87,8 +99,33 @@ function App() {
             </Search>
           </Toolbar>
         </AppBar>
+        <Box sx={{ p: 1, pb: 0 }}>
+          <Stack direction="row" spacing={1}>
+            <Chip label="Clickable" onClick={handleClick} />
+            <Chip label="Clickable" variant="outlined" onClick={handleClick} />
+          </Stack>
+        </Box>
+        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Trash" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Spam" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
       </Box>
-      hogeho
     </div>
   );
 }
